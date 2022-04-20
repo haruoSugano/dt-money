f707ff910a44953722d06b842fa341b831a27f29
haruoSugano <haruosugano@icloud.com>
2022-04-20 00:26:42 -0300

    Finalizado

---
+++ src/components/Dashbord/index.tsx
@@ -0,0 +1,12 @@
+import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
+import { Container } from "./styles";
+
+export function Dashbord() {
+    return(
+        <Container>
+            <Summary />
+            <TransactionsTable />
+        </Container>
+    );
+}
